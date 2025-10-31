7b697538f745d4a0bcc2103446ea02e9a73ad42c
Xavier Liu <huami.xavier@gmail.com>
2025-10-31 07:37:36 +0000

    update

--- pages/RewardList/jsobjects/MyObject/MyObject.js
+++ pages/RewardList/jsobjects/MyObject/MyObject.js
@@ -1,9 +1,44 @@
 export default {
-	myVar1: [
+	rewardList: [
 			   {
                 "id": 1,
                 "type": "STEPS",
                 "backendConfigTitle":"ABC",
+                "ctype": 1,
+                "appname": "com.huami.midong",
+                "difficulty": 2,
+                "contents": {
+                    "title": "每日万步挑战",
+                    "description": "完成每日10000步目标",
+                    "ruleDesc": "在24小时内完成10000步"
+                },
+                "assets": {
+                    "bgActive": "https://assets.challenge_active.png",
+                    "bgInactive": "https://assets.challenge_inactive.png",
+                    "icon": "https://assets.challenge_icon.png"
+                },
+                "repeatPeriod": 0,
+                "order": 1,
+                "startDate": 1698765432,
+                "endDate": 1701357432,
+                "onlineDate": 1698679032,
+                "offlineDate": 1701443832,
+                "rules": [
+                    {
+                        "id": 1,
+                        "expression": "steps >= 10000",
+                        "rewardConfig": {
+                            "type": "points",
+                            "amount": 100,
+                            "badgeId": 5
+                        }
+                    }
+                ]
+      },
+		  {
+                "id": 2,
+                "type": "STEPS",
+                "backendConfigTitle":"DEF",
                 "ctype": 1,
                 "appname": "com.huami.midong",
                 "difficulty": 2,
