a58fdda6a847034012b8ec0cc527b12b17d023d2
Xavier Liu <huami.xavier@gmail.com>
2025-11-03 07:19:58 +0000

    abc

---
+++ pages/ChallengeDetail/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,82 @@
+export default {
+	challenge: {
+                "id": 1,
+                "type": "STEPS",
+                "backendConfigTitle":"ABC",
+                "ctype": 1,
+                "appname": "com.huami.midong",
+                "difficulty": 2,
+                "contents": {
+                    "title": "每日万步挑战",
+                    "description": "完成每日10000步目标",
+                    "ruleDesc": "在24小时内完成10000步"
+                },
+                "assets": {
+                    "bgActive": "https://assets.challenge_active.png",
+                    "bgInactive": "https://assets.challenge_inactive.png",
+                    "icon": "https://assets.challenge_icon.png"
+                },
+                "repeatPeriod": 0,
+                "order": 1,
+                "startDate": 1698765432,
+                "endDate": 1701357432,
+                "onlineDate": 1698679032,
+                "offlineDate": 1701443832,
+                "rules": [
+                    {
+                        "id": 1,
+                        "expression": "steps >= 10000",
+                        "rewardConfig": {
+                            "type": "points",
+                            "amount": 100,
+                            "badgeId": 5
+                        }
+                    }
+                ]
+  },
+	sportType:
+	[
+  	{
+    	"name": "STEPS",
+    	"code": "STEPS"
+  	},
+  	{
+    	"name": "RUN",
+    	"code": "RUN"
+  	},
+  	{
+    	"name": "RECOVERY",
+    	"code": "RECOVERY"
+  	}
+	],
+	difficultyLevel:
+	[
+  	{
+    	"name": "Very Easy",
+    	"code": 1
+  	},
+  	{
+    	"name": "Easy",
+    	"code": 2
+  	},
+  	{
+    	"name": "Medium",
+    	"code": 3
+  	},
+		{
+    	"name": "Hard",
+    	"code": 4
+  	}
+	],
+	
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
---
+++ pages/ChallengeList/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,84 @@
+export default {
+	challenges: [
+            {
+                "id": 1,
+                "type": "STEPS",
+                "backendConfigTitle":"ABC",
+                "ctype": 1,
+                "appname": "com.huami.midong",
+                "difficulty": 2,
+                "contents": {
+                    "title": "每日万步挑战",
+                    "description": "完成每日10000步目标",
+                    "ruleDesc": "在24小时内完成10000步"
+                },
+                "assets": {
+                    "bgActive": "https://assets.challenge_active.png",
+                    "bgInactive": "https://assets.challenge_inactive.png",
+                    "icon": "https://assets.challenge_icon.png"
+                },
+                "repeatPeriod": 0,
+                "order": 1,
+                "startDate": 1698765432,
+                "endDate": 1701357432,
+                "onlineDate": 1698679032,
+                "offlineDate": 1701443832,
+                "rules": [
+                    {
+                        "id": 1,
+                        "expression": "steps >= 10000",
+                        "rewardConfig": {
+                            "type": "points",
+                            "amount": 100,
+                            "badgeId": 5
+                        }
+                    }
+                ]
+            },
+		        {
+                "id": 2,
+                "type": "STEPS",
+                "backendConfigTitle":"DEF",
+                "ctype": 1,
+                "appname": "com.huami.midong",
+                "difficulty": 2,
+                "contents": {
+                    "title": "每周万步挑战",
+                    "description": "完成每周10000步目标",
+                    "ruleDesc": "在周内完成10000步"
+                },
+                "assets": {
+                    "bgActive": "https://assets.challenge_active.png",
+                    "bgInactive": "https://assets.challenge_inactive.png",
+                    "icon": "https://assets.challenge_icon.png"
+                },
+                "repeatPeriod": 0,
+                "order": 1,
+                "startDate": 1698765432,
+                "endDate": 1701357432,
+                "onlineDate": 1698679032,
+                "offlineDate": 1701443832,
+                "rules": [
+                    {
+                        "id": 1,
+                        "expression": "steps >= 10000",
+                        "rewardConfig": {
+                            "type": "points",
+                            "amount": 100,
+                            "badgeId": 5
+                        }
+                    }
+                ]
+            }
+  ],
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+	}
+}
