be4fff98a593ba076b858e274a11e6a6e7ff8190
Xavier Liu <huami.xavier@gmail.com>
2025-11-11 03:00:34 +0000

    abc

--- pages/BadgeList/jsobjects/MyObject/MyObject.js
+++ pages/BadgeList/jsobjects/MyObject/MyObject.js
@@ -1,82 +1,155 @@
 export default {
+	badges: [
+		{
+			"id": 1,
+			"backendConfigTitle": "ABC",
+			"parentId": 0,
+			"appname": "com.midong.huami",
+			"contents": {
+				"badge_title": "title_key1",
+				"badge_description": "description_key1",
+				"badge_tag_challenge": [
+					"tag_key1",
+					"tag_key2"
+				]
+			},
+			"assets": {
+				"icon": "badges/running_active.png"
+			},
+			"type": 0,
+			"actionType": 1,
+			"predictExpression": "distance >= 1000",
+			"goalExpression": "1000",
+			"progressExpression": "distance>1000",
+			"showPredict": "version>10.0.0",
+			"mark": 1,
+			"level": 1,
+			"difficulty": 1,
+			"repeatReriod": 1,
+			"isRepeatable": 1,
+			"visibilityStartTime": "2023-10-01 10:00:00",
+			"visibilityEndTime": "2023-10-01 10:00:00",
+			"availabilityStartTime": "2023-10-01 10:00:00",
+			"availabilityEndTime": "2023-10-01 10:00:00",
+			"status": 1,
+			"groupId": "xxxxxxx"
+		},
+		{
+			"id": 2,
+			"backendConfigTitle": "BBC",
+			"parentId": 0,
+			"appname": "com.midong.huami",
+			"contents": {
+				"badge_title": "title_key1",
+				"badge_description": "description_key1",
+				"badge_tag_challenge": [
+					"tag_key1",
+					"tag_key2"
+				]
+			},
+			"assets": {
+				"icon": "badges/running_active.png"
+			},
+			"type": 0,
+			"actionType": 1,
+			"predictExpression": "distance >= 1000",
+			"goalExpression": "1000",
+			"progressExpression": "distance>1000",
+			"showPredict": "version>10.0.0",
+			"mark": 1,
+			"level": 1,
+			"difficulty": 1,
+			"repeatReriod": 1,
+			"isRepeatable": 1,
+			"visibilityStartTime": "2023-10-01 10:00:00",
+			"visibilityEndTime": "2023-10-01 10:00:00",
+			"availabilityStartTime": "2023-10-01 10:00:00",
+			"availabilityEndTime": "2023-10-01 10:00:00",
+			"status": 1,
+			"groupId": "xxxxxxx"
+		}
+
+	],
+
+
 	rewardList: [
-							{
-                    "id": 1,
-                    "parentId": 0,
-                    "appname": "com.midong.huami",
-                    "contents": {
-                        "badge_title": "title_key1",
-                        "badge_description": "description_key1",
-                        "badge_tag_challenge": [
-                            "tag_key1",
-                            "tag_key2"
-                        ]
-                    },
-                    "assets": {
-                        "icon": "badges/running_active.png"
-                    },
-                    "predictExpression": "distance >= 1000",
-                    "type": 0,
-                    "actiontype": 1,
-                    "goalExpression": "1000",
-                    "progressExpression": "distance",
-                    "showPredict": "version>10.0.0",
-                    "mark": 1,
-                    "visibilityStartTime": "2023-10-01 10:00:00",
-                    "visibilityEndTime": "2023-10-01 10:00:00",
-                    "availabilityStartTime": "2023-10-01 10:00:00",
-                    "availabilityEndTime": "2023-10-01 10:00:00",
-                    "level": 1,
-                    "difficulty": 1,
-                    "repeatReriod": 1,
-                    "isRepeatable": 1,
-                    "createdTime": "2023-10-01 10:00:00",
-                    "updatedTime": "2023-10-01 10:00:00",
-                    "createBy": "zhangtao1@zepp.com",
-                    "updateBy": "zhangtao1@zepp.com",
-                    "status": 1,
-                    "groupId": "xxxxxxx",
-                    "badgeStatus": 1,
-                    "isDelete": 1
-                },
-								{
-                    "id": 2,
-                    "parentId": 0,
-                    "appname": "com.midong.huami",
-                    "contents": {
-                        "badge_title": "title_key1",
-                        "badge_description": "description_key1",
-                        "badge_tag_challenge": [
-                            "tag_key1",
-                            "tag_key2"
-                        ]
-                    },
-                    "assets": {
-                        "icon": "badges/running_active.png"
-                    },
-                    "predictExpression": "distance >= 1000",
-                    "type": 0,
-                    "actiontype": 1,
-                    "goalExpression": "1000",
-                    "progressExpression": "distance",
-                    "showPredict": "version>10.0.0",
-                    "mark": 1,
-                    "visibilityStartTime": "2023-10-01 10:00:00",
-                    "visibilityEndTime": "2023-10-01 10:00:00",
-                    "availabilityStartTime": "2023-10-01 10:00:00",
-                    "availabilityEndTime": "2023-10-01 10:00:00",
-                    "level": 1,
-                    "difficulty": 1,
-                    "repeatReriod": 1,
-                    "isRepeatable": 1,
-                    "createdTime": "2023-10-01 10:00:00",
-                    "updatedTime": "2023-10-01 10:00:00",
-                    "createBy": "zhangtao1@zepp.com",
-                    "updateBy": "zhangtao1@zepp.com",
-                    "status": 1,
-                    "groupId": "xxxxxxx",
-                    "badgeStatus": 1,
-                    "isDelete": 1
-                }
+		{
+			"id": 1,
+			"parentId": 0,
+			"appname": "com.midong.huami",
+			"contents": {
+				"badge_title": "title_key1",
+				"badge_description": "description_key1",
+				"badge_tag_challenge": [
+					"tag_key1",
+					"tag_key2"
+				]
+			},
+			"assets": {
+				"icon": "badges/running_active.png"
+			},
+			"predictExpression": "distance >= 1000",
+			"type": 0,
+			"actiontype": 1,
+			"goalExpression": "1000",
+			"progressExpression": "distance",
+			"showPredict": "version>10.0.0",
+			"mark": 1,
+			"visibilityStartTime": "2023-10-01 10:00:00",
+			"visibilityEndTime": "2023-10-01 10:00:00",
+			"availabilityStartTime": "2023-10-01 10:00:00",
+			"availabilityEndTime": "2023-10-01 10:00:00",
+			"level": 1,
+			"difficulty": 1,
+			"repeatReriod": 1,
+			"isRepeatable": 1,
+			"createdTime": "2023-10-01 10:00:00",
+			"updatedTime": "2023-10-01 10:00:00",
+			"createBy": "zhangtao1@zepp.com",
+			"updateBy": "zhangtao1@zepp.com",
+			"status": 1,
+			"groupId": "xxxxxxx",
+			"badgeStatus": 1,
+			"isDelete": 1
+		},
+		{
+			"id": 2,
+			"parentId": 0,
+			"appname": "com.midong.huami",
+			"contents": {
+				"badge_title": "title_key1",
+				"badge_description": "description_key1",
+				"badge_tag_challenge": [
+					"tag_key1",
+					"tag_key2"
+				]
+			},
+			"assets": {
+				"icon": "badges/running_active.png"
+			},
+			"predictExpression": "distance >= 1000",
+			"type": 0,
+			"actiontype": 1,
+			"goalExpression": "1000",
+			"progressExpression": "distance",
+			"showPredict": "version>10.0.0",
+			"mark": 1,
+			"visibilityStartTime": "2023-10-01 10:00:00",
+			"visibilityEndTime": "2023-10-01 10:00:00",
+			"availabilityStartTime": "2023-10-01 10:00:00",
+			"availabilityEndTime": "2023-10-01 10:00:00",
+			"level": 1,
+			"difficulty": 1,
+			"repeatReriod": 1,
+			"isRepeatable": 1,
+			"createdTime": "2023-10-01 10:00:00",
+			"updatedTime": "2023-10-01 10:00:00",
+			"createBy": "zhangtao1@zepp.com",
+			"updateBy": "zhangtao1@zepp.com",
+			"status": 1,
+			"groupId": "xxxxxxx",
+			"badgeStatus": 1,
+			"isDelete": 1
+		}
 	]
 }
